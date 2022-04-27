851821f2a57baa4a3e53b5b0862be472342606fc
belen ardiles <belenardiles11@gmail.com>
2022-04-26 21:40:53 -0300

    antes del reset

---
+++ src/main/java/ProyectoFinal/example/Hospital/Controladores/FrontControlador.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProyectoFinal.example.Hospital.Controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/front")
+public class FrontControlador {
+    @GetMapping("/secretariaPrincipal")
+    public String secretariaPrincipal(){
+        return "secretariaPrincipal";
+    }
+    
+    @GetMapping("/registroPaciente")
+    public String registroPaciente(){
+        return "registroPaciente";
+    }
+   
+      @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
